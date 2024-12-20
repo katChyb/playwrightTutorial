e6d93f2096f964434b8be38802c70297abc6bf36
katarzyna chybczynska <katarzyna_chybczynska@10g.pl>
2024-12-20 14:25:29 +0100

    first test with assertion and xpath

--- test1_login.py
+++ test1_login.py
@@ -6,7 +6,7 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.pause()
+#    page.pause()
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.wait_for_load_state("networkidle")
     page.get_by_role("button", name="Log In").click()
@@ -19,6 +19,11 @@ def run(playwright: Playwright) -> None:
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     page.wait_for_load_state("networkidle")
     expect( page.get_by_role("button", name="Log In")).to_be_hidden()
+# locator("xpath=//div[contains(@class, 'p_m9YY aG5eBy')]").first
+    product = page.get_by_text("$85").first.locator("xpath=../../../../../../../../../..").text_content()
+    assert product != "Socks"
+
+
     print("yay")
 
     # ---------------------
