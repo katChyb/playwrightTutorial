79afe02d8c559252132c0e3fab0cc33375b1f9b4
katarzyna chybczynska <katarzyna_chybczynska@10g.pl>
2025-01-02 15:55:09 +0100

    iterating links on page

--- test1_login.py
+++ test1_login.py
@@ -12,16 +12,25 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("button", name="Log In").click()
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_role("button", name="Log in with Email").click()
-    page.get_by_test_id("emailAuth").get_by_label("Email").click()
-    page.get_by_test_id("emailAuth").get_by_label("Email").fill("korin666@o2.pl")
+ #   page.get_by_test_id("emailAuth").get_by_label("Email").click()
+ #   page.get_by_test_id("emailAuth").get_by_label("Email").fill("korin666@o2.pl")
+    page.fill('input:below(:text("Email"))', "korin666@o2.pl")
     page.get_by_label("Password").click()
     page.get_by_label("Password").fill("test1")
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     page.wait_for_load_state("networkidle")
     expect( page.get_by_role("button", name="Log In")).to_be_hidden()
+    page.pause()
+
+    all_links = page.get_by_role("link").all()
+    for link in all_links:
+        if link.text_content() == "$85":
+            assert 'socks' not in link.text_content()
+
+
 # locator("xpath=//div[contains(@class, 'p_m9YY aG5eBy')]").first
-    product = page.get_by_text("$85").first.locator("xpath=../../../../../../../../../..").text_content()
-    assert product != "Socks"
+#    product = page.get_by_text("$85").first.locator("xpath=../../../../../../../../../..").text_content()
+#    assert product != "Socks"
 
 
     print("yay")
