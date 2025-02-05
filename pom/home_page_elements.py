918fedb3329078d636205c41bd97cddff4ae7686
katarzynachybczynska <katarzyna_chybczynska@10g.pl>
2025-02-05 14:19:56 +0100

    changes from 05.02.2025 capturing  full page

--- pom/home_page_elements.py
+++ pom/home_page_elements.py
@@ -1,9 +1,12 @@
+import page
+
 
 class HomePage:
 
 
         celebrate_header = "text= Celebrating Beauty and Style"
         celebrate_body = "text= playwright-practice was founded by a group of like-minded fashion"
+
 
     # def __init__(self, page):
     #
--- visual_tests/test_landing_page_visual.py
+++ visual_tests/test_landing_page_visual.py
@@ -1,8 +1,21 @@
 import time
+from unittest.result import failfast
 
-from playwright.sync_api import Playwright, sync_playwright, expect
+import pytest
 
+
+@pytest.mark.xfail
 def test_visual_landing(page, assert_snapshot) -> None:
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.wait_for_load_state("networkidle")
-    assert_snapshot(page.screenshot())
+    assert_snapshot(page.screenshot)
+
+# this screenshot will have full page view
+@pytest.mark.xfail
+def test_visual_landing__full_page_snap(page, assert_snapshot) -> None:
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    page.wait_for_load_state("networkidle")
+    assert_snapshot(page.screenshot(full_page=True))
+   # assert_snapshot(page.screenshot(full_page=True), fail_fast=True)  this will throw fail after spotting first not
+   # matching pixel, allowing to have errorst information faster
+
