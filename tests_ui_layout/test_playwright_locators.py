dbbd8e7c3460b91cbb4b93a13e8a365bb0147e88
katarzynachybczynska <katarzyna_chybczynska@10g.pl>
2025-02-07 11:54:11 +0100

    changes from 07.02.2025 updating locators test and visual test

--- tests_ui_layout/test_playwright_locators.py
+++ tests_ui_layout/test_playwright_locators.py
@@ -3,20 +3,35 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def test_run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
+    browser = playwright.chromium.launch(headless=True, slow_mo=400)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.set_default_timeout(3000)
-    # page.wait_for_load_state("networkidle")
     # playwright locators https://playwright.dev/python/docs/locators
     # https://www.w3schools.com/xml/xpath_axes.asp
-    page.click("text= Log In")
+    page.wait_for_load_state("networkidle")
+
+    #examples of locators
+    # xpath by class   locator('xpath= //wow-image')
+    # page.locator("text= shop").first  .nth(0)
+    # page.locator("xpath=//*[contains(@class, 'naMHY_vALCqq')]").first / nth(0)
     # in console you can use eg this CSS selector to identify your element: playwright.$("input[type= "email"]") or
     # playwright.$("text= Shop") or playwright.$(":nth-match(:text('Shop'), 1)") or playwright.$(":nth-match(button, 2)")
     # or playwright.$("button:has-text('Log in')")
-    #page.click("'Log in'", timeout= 3000)    not working for me
+
+    all_links= page.get_by_role("link").all()
+    for link in all_links:
+       if link.text_content() =='$85':
+           assert 'socks' not in link.text_content() and 'notepad' not in link.text_content()
+
+    # product = page.get_by_text('$85').first.locator('xpath=../../../../..').text_content()
+    # print(product)
+    # expect(product).not_to_contain_text("Socks")
+    # assert product != "100"
     print("yay")
+
+    #input:below(:text("Email")) email input on lohin page  in console you can use playwright.$('input:below(:text("Email"))')
 
     # ---------------------
     context.close()
--- visual_tests/test_landing_page_visual.py
+++ visual_tests/test_landing_page_visual.py
@@ -17,5 +17,5 @@ def test_visual_landing__full_page_snap(page, assert_snapshot) -> None:
     page.wait_for_load_state("networkidle")
     assert_snapshot(page.screenshot(full_page=True))
    # assert_snapshot(page.screenshot(full_page=True), fail_fast=True)  this will throw fail after spotting first not
-   # matching pixel, allowing to have errorst information faster
+   # matching pixel, allowing to have error information faster
 
